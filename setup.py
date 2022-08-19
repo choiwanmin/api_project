557b5da08b4e80e0ab0ebd45be194e001ce3b047
playdata <wmchoi250@gmail.com>
2022-08-19 12:22:07 +0900

    add: setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+# setup.py 파일이 있으면 pip로 설치 가능하다.
+from setuptools import setup
+
+setup(
+    name = "myapi", # 이 이름으로 패키지가 설치
+    version = "0.0.1", # 릴리즈,메이저,마이너 /넘버
+    description = "my api lib",
+    url = "https://github.com/choiwanmin/api_project.git",
+    author = "wmchoi",
+    author_email = "wmchoi250@gmail.com",
+    package = ["my_api"],
+    install_requires = [
+        "requests"
+    ]
+)
